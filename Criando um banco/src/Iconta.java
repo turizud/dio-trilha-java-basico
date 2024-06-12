68a726e9aef0f1f430ba707b7f7a4aac3a18f68a
Érika Oliveira Souza <trizdiwyne8420@gmail.com>
2024-06-12 17:49:34 -0300

    Criação do banco digital.

---
+++ Criando um banco/src/Iconta.java
@@ -0,0 +1,14 @@
+public interface Iconta {
+
+    
+	
+        void sacar(double valor);
+        
+        void depositar(double valor);
+        
+        void transferir(double valor, Iconta contaDestino);
+        
+        void imprimirExtrato();
+    
+    
+}
